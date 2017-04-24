f87b473473c75db927aac41ae9d933c79f4cd646
euand <euan.dowers@barcelonagse.eu>
2017-04-24 22:22:28 +0200

    Fixed variable scope (nested functions)

--- HW2/gibbs.py
+++ HW2/gibbs.py
@@ -10,33 +10,35 @@ os.chdir('/home/euan/documents/text-mining/BGSE_text_mining')
 data = pd.read_table("HW1/speech_data_extend.txt",encoding="utf-8")
 stemmed, processed_data = data_processing(data)
 
-
-def Z_probs(Beta, Theta):
-    Z = [[Theta[i,:]*Beta[:,idx[word]]/Theta[i,:].dot(Beta[:,idx[word]]) for word in stemmed[i] ] for i in range(Theta.shape[0])]
-    return Z
-
-def Beta_sample(eta, Z):
-    z_s = [np.argmax(i) for sublist in Z for i in sublist ]
-    M = np.zeros(shape=(K,V))
-    for k in range(K):
-        words = [s[i] for i in range(len(z_s)) if z_s[i] == k]
-        counts = Counter(words)
-        for word in set(words):
-            M[k,idx[word]] = counts[word]
-    Beta = [dirichlet(alpha = eta + M[i],size = 1)[0] for i in range(K)]
-    return np.array(Beta)
-
-def Theta_sample(alpha, Z):
-    N = np.zeros(shape=(D,K))
-    for i in range(D):
-        Z_s = [np.argmax(i) for i in Z[i]]
-        counts = Counter(Z_s)
-        for j in set(counts.keys()):
-            N[i,j] = counts[j]
-    Beta = [dirichlet(alpha = alpha + N[i],size = 1)[0] for i in range(D)]
-    return Theta
-
 def Gibbs_sampling_LDA(stemmed, K, alpha, eta, m=5, iters = 200):
+
+    def Z_probs(Beta, Theta):
+        Z = [[Theta[i,:]*Beta[:,idx[word]]/Theta[i,:].dot(Beta[:,idx[word]]) for word in stemmed[i] ] for i in range(Theta.shape[0])]
+        return Z
+
+    def Beta_sample(eta, Z):
+        K   = len(Z[0][0])
+        z_s = [np.argmax(i) for sublist in Z for i in sublist ]
+        M = np.zeros(shape=(K,V))
+        for k in range(K):
+            words = [s[i] for i in range(len(z_s)) if z_s[i] == k]
+            counts = Counter(words)
+            for word in set(words):
+                M[k,idx[word]] = counts[word]
+        Beta = [dirichlet(alpha = eta + M[i],size = 1)[0] for i in range(K)]
+        return np.array(Beta)
+
+    def Theta_sample(alpha, Z):
+        K   = len(Z[0][0])
+        N   = np.zeros(shape=(D,K))
+        for i in range(D):
+            Z_s     = [np.argmax(i) for i in Z[i]]
+            counts  = Counter(Z_s)
+            for j in set(counts.keys()):
+                N[i,j]  = counts[j]
+        Theta = [dirichlet(alpha = alpha + N[i],size = 1)[0] for i in range(D)]
+        return np.array(Theta)
+
     s       = [i for sublist in stemmed for i in sublist ] # list of all words in corpus (not unique)
     vocab   = get_vocab(stemmed)
     D       = len(stemmed)
@@ -59,3 +61,5 @@ def Gibbs_sampling_LDA(stemmed, K, alpha, eta, m=5, iters = 200):
             Output.append(Z_s)
 
     return Output
+
+z = Gibbs_sampling_LDA(stemmed, K = 10, alpha = 1, eta = 1, m = 5, iters = 200)
