a04e29df2105f91369307758d2dfd155edbe3404
euand <euan.dowers@barcelonagse.eu>
2017-04-29 18:44:09 +0200

    Improved sampling speed

--- HW2/gibbs.py
+++ HW2/gibbs.py
@@ -4,7 +4,6 @@ import nltk
 import os
 import scipy.sparse as ssp
 import time
-
 from numpy.random import dirichlet
 from utils import data_processing, get_vocab, make_count
 from collections import Counter
@@ -12,8 +11,8 @@ from collections import Counter
 os.chdir('/home/euan/documents/text-mining/BGSE_text_mining/')
 
 data = pd.read_table("HW1/speech_data_extend.txt",encoding="utf-8")
-data_post1900 = data.loc[data.year >= 1900]
-stemmed, processed_data = data_processing(data_post1900)
+data_post1945 = data.loc[data.year >= 1945]
+%time stemmed, processed_data = data_processing(data_post1945)
 
 def Gibbs_sampling_LDA(stemmed, K, alpha = None, eta = None, m=3, iters = 200, burnin = 500, perplexity = False):
     '''
@@ -25,8 +24,12 @@ def Gibbs_sampling_LDA(stemmed, K, alpha = None, eta = None, m=3, iters = 200, b
     #    Z = [ [Theta[i,:]*Beta[:,idx[word]]/denoms[i,idx[word]] for word in stemmed[i] ] for i in range(Theta.shape[0])]
     #    return Z
 
-    def Z_class(Beta, Theta):
-        Z = [[ np.argmax(Theta[i,:]*Beta[:,idx[word]]) for word in stemmed[i] ] for i in range(Theta.shape[0])]
+    #def Z_class(Beta, Theta):
+    #    Z = [[ np.argmax(Theta[i,:]*Beta[:,idx[word]]) for word in stemmed[i] ] for i in range(Theta.shape[0])]
+    #    return Z
+
+    def Z_class_1(Beta, Theta):
+        Z = [np.ndarray.tolist( np.argmax( Beta[:,[idx[word] for word in stemmed[i]]] * Theta[i,:].reshape((K, 1)), axis = 0) ) for i in range(Theta.shape[0] )]
         return Z
 
     #%time Z = Z_class(Beta, Theta)
@@ -42,8 +45,6 @@ def Gibbs_sampling_LDA(stemmed, K, alpha = None, eta = None, m=3, iters = 200, b
         Beta = [dirichlet(alpha = eta + M[i],size = 1)[0] for i in range(K)]
         return np.array(Beta)
 
-    #%time  Beta = Beta_sample(eta, Z)
-
     def Theta_sample(alpha, Z):
         N   = np.zeros(shape=(D,K))
         for i in range(D):
@@ -52,8 +53,6 @@ def Gibbs_sampling_LDA(stemmed, K, alpha = None, eta = None, m=3, iters = 200, b
                 N[i,j]  = counts[j]
         Theta = [dirichlet(alpha = alpha + N[i],size = 1)[0] for i in range(D)]
         return np.array(Theta)
-
-    #%time Theta =  Theta_sample(alpha, Z)
 
     def onehotencode(Z):
         '''
@@ -73,8 +72,6 @@ def Gibbs_sampling_LDA(stemmed, K, alpha = None, eta = None, m=3, iters = 200, b
         denom   = len(s)
         return np.exp(-num/denom)
 
-    #%time perp = perplexity(Theta,Beta,count_matrix)
-
     # Get params needed for passing to sampling functions
     s       = [i for sublist in stemmed for i in sublist ]
     vocab   = get_vocab(stemmed)
@@ -92,31 +89,37 @@ def Gibbs_sampling_LDA(stemmed, K, alpha = None, eta = None, m=3, iters = 200, b
 
     Theta   = dirichlet(alpha = [alpha]*K, size = D)
     Beta    = dirichlet(alpha = [eta]*V, size = K)
-    Z       = Z_class(Beta, Theta)
+    Z       = Z_class_1(Beta, Theta)
     labels  = ssp.coo_matrix(onehotencode(Z))
 
     # SAMPLING
+    print('TIME:', time.strftime("%H:%M:%S", time.gmtime()))
     for i in range(burnin):
-        Z       = Z_class(Beta, Theta)
+        Z       = Z_class_1(Beta, Theta)
         Beta    = Beta_sample(eta, Z)
         Theta   = Theta_sample(alpha, Z)
         if i%20 == 0:
             if perplexity:
                 perp.append(perplexity(Theta, Beta, count_matrix))
+            print('Burnin iteration {}'.format(i))
+
+    print('TIME:', time.strftime("%H:%M:%S", time.gmtime()))
     for i in range(iters):
-        Z       = Z_class(Beta, Theta)
+        Z       = Z_class_1(Beta, Theta)
         Beta    = Beta_sample(eta, Z)
         Theta   = Theta_sample(alpha, Z)
+
         # Add every m-th sample to output
         if i%m == 0:
             labels  += ssp.coo_matrix(onehotencode(Z))
         if i%20 == 0:
             if perplexity:
                 perp.append(perplexity(Theta, Beta, count_matrix))
+            print( "Iteration {}".format(i))
 
     return (labels, perp)
 
-%time LDA_labels, perp = Gibbs_sampling_LDA(stemmed, K = 10, iters = 10, perplexity=True, burnin = 1)
+%time LDA_labels, perp = Gibbs_sampling_LDA(stemmed, K = 10, iters = 2000, perplexity=True, burnin = 500)
 
 LDA_labels = pd.DataFrame(LDA_labels.toarray())
 pd.DataFrame.to_csv(LDA_labels,path_or_buf='LDA_labels.csv',index=False)
@@ -124,6 +127,8 @@ pd.DataFrame.to_csv(LDA_labels,path_or_buf='LDA_labels.csv',index=False)
 perp =  pd.DataFrame(perp)
 pd.DataFrame.to_csv(perp,path_or_buf='perp.csv',index=False)
 
+from matplotlib import pyplot as plt
+plt.plot(perp)
 
 
 ##############################################################
@@ -135,5 +140,5 @@ import lda
 idx = dict(zip(get_vocab(stemmed),range(len(get_vocab(stemmed)))))
 X   = make_count(stemmed, idx)
 X   = X.astype(int)
-model = lda.LDA(n_topics= 10, n_iter=1000)
+model = lda.LDA(n_topics= 10, n_iter=2500)
 %time model.fit(X)
